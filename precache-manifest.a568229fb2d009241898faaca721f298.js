028a7a5c59e6253c7b5500670f01acabfc9e4aa8
abid7 <abidshahriar7@gmail.com>
2020-08-29 19:42:26 +0600

    Updates

---
+++ precache-manifest.a568229fb2d009241898faaca721f298.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "440259bb2ba766c3672dd8e900ebfc46",
+    "url": "/react-css-position-gui/index.html"
+  },
+  {
+    "revision": "cf633bf0730f63692a70",
+    "url": "/react-css-position-gui/static/css/main.a37cd410.chunk.css"
+  },
+  {
+    "revision": "80a51acc19e129dfdd9d",
+    "url": "/react-css-position-gui/static/js/2.d2dd5d47.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-css-position-gui/static/js/2.d2dd5d47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf633bf0730f63692a70",
+    "url": "/react-css-position-gui/static/js/main.f0f3ae67.chunk.js"
+  },
+  {
+    "revision": "958867ba1a29f6360f2c",
+    "url": "/react-css-position-gui/static/js/runtime-main.254dcbc2.js"
+  }
+]);
